a8d80a3ec2d02106929788cd54643f86a0e102e0
resteve <resteve@zikzakmedia.com>
2015-03-18 14:20:57 +0100

    Get shipments (with lock table)

--- cart.py
+++ cart.py
@@ -4,6 +4,9 @@
 from trytond.model import ModelView, ModelSQL, fields
 from trytond.pool import Pool, PoolMeta
 from trytond.transaction import Transaction
+from trytond.rpc import RPC
+
+import logging
 
 __all__ = ['StockCart', 'StockShipmentOutCart']
 __metaclass__ = PoolMeta
@@ -19,6 +22,13 @@ class StockCart(ModelSQL, ModelView):
         help='Number of columns are available in this cart')
     active = fields.Boolean('Active')
 
+    @classmethod
+    def __setup__(cls):
+        super(StockCart, cls).__setup__()
+        cls.__rpc__.update({
+            'get_shipments': RPC(readonly=False),
+            })
+
     @staticmethod
     def default_active():
         return True
@@ -31,27 +41,71 @@ class StockCart(ModelSQL, ModelView):
     def default_columns():
         return 1
 
-    def get_shipments(self):
-        Picking = Pool().get('stock.shipment.out')
-        user = Transaction().user
-        basket = self.rows * self.columns
+    @classmethod
+    def get_shipments(cls, warehouse=None, state=['assigned'], attempts=0, total_attempts=5):
+        '''
+        Return a list shipments
+        @param warehouse: ID warehouse domain to search shipments
+        @param state: list. Shipment states to filter
+        @param attempts: int. Attempts when table is lock
+        @param total_attempts: int. Total attempts to try get shipments unlock
+        '''
+        pool = Pool()
+        Shipment = pool.get('stock.shipment.out')
+        Carts = pool.get('stock.shipment.out.cart')
+        User = pool.get('res.user')
 
-        shipments = Picking.search([
-                ('state', '=', 'assigned'),
-                ('stock_cart', '=', True),
-                ('cart_user', '=', user),
-                ('cart_shipment', '=', True),
-                ], limit=basket, order='planned_date asc')
-        if shipments:
-            return shipments
+        transaction = Transaction()
+        user = User(Transaction().user)
 
-        shipments = Picking.search([
-                ('state', '=', 'assigned'),
-                ('stock_cart', '=', True),
-                ('cart', '=', None),
-                ('cart_shipment', '=', False),
-                ], limit=basket, order='planned_date asc')
-        return shipments
+        if not user.cart:
+            logging.getLogger('Stock Cart').warning(
+                'User %s not have cart in their preferences' % user.rec_name)
+            return []
+        baskets = user.cart.rows * user.cart.columns
+
+        domain = [('state', 'in', state)]
+        if warehouse:
+            domain.append(('warehouse', '=', warehouse))
+
+        try:
+            # Locks transaction. Nobody can query this table
+            transaction.cursor.lock(Carts._table)
+        except:
+            # Table is locked. Captures operational error and returns void list
+            if attempts < total_attempts:
+                cls.get_shipments(state, attempts+1, total_attempts)
+            else:
+                logging.getLogger('Stock Cart').warning(
+                    'Table Carts is lock after %s attempts' % (total_attempts))
+                return []
+        else:
+            # if there are carts state draft, return first this carts
+            carts = Carts.search_read([
+                ('state', '=', 'draft'),
+                ('user', '=', user),
+                ], limit=baskets)
+            if carts:
+                return carts
+
+            # Assign new shipments
+            shipments = [s.id for s in Shipment.search(domain, order=[('planned_date', 'ASC')])]
+
+            carts_assigned = [c.id for c in Carts.search([
+                ('shipment', 'in', shipments),
+                ])]
+
+            #TODO. Respect order shipments
+            shipments_cart = list(set(shipments) - set(carts_assigned))
+
+            # Save carts assigned to user
+            to_create = []
+            for s in shipments_cart[:baskets]:
+                to_create.append({'shipment': s})
+            if to_create:
+                carts = Carts.create(to_create)
+                return carts
+        return []
 
 
 class StockShipmentOutCart(ModelSQL, ModelView):
@@ -64,6 +118,18 @@ class StockShipmentOutCart(ModelSQL, ModelView):
         ('draft', 'Draft'),
         ('done', 'Done'),
         ], 'State', readonly=True)
+
+    @classmethod
+    def __setup__(cls):
+        super(StockShipmentOutCart, cls).__setup__()
+        cls._sql_constraints += [
+            ('shipment_uniq', 'UNIQUE(shipment)',
+                'The shipment must be unique!'),
+            ]
+        cls._order.insert(0, ('shipment', 'DESC'))
+        cls._error_messages.update({
+            'shipment_uniq': 'The shipment must be unique!',
+            })
 
     @staticmethod
     def default_state():
