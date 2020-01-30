e173380e7c1eb548d93138f95b018e104527c844
Raimon Esteve <raimonesteve@gmail.com>
2020-01-30 14:58:27 +0100

    Add create issue option in stock configuration

--- __init__.py
+++ __init__.py
@@ -2,6 +2,7 @@
 # The COPYRIGHT file at the top level of this repository contains
 # the full copyright notices and license terms.
 from trytond.pool import Pool
+from . import configuration
 from . import cart
 from . import inventory
 from . import user
@@ -9,6 +10,7 @@ from . import user
 
 def register():
     Pool.register(
+        configuration.Configuration,
         cart.StockCart,
         cart.StockShipmentOutCart,
         cart.StockShipmentOutCartLine,
